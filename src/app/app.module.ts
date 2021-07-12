d707b9a1dc48e5f0d2e86296e7ea4221332ed2df
Chonlasith Jucksriporn <chonlasith@gmail.com>
2021-07-12 14:47:08 +0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TimerComponent } from './components/timer/timer.component';
+import { ControllerComponent } from './components/controller/controller.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerComponent,
+    ControllerComponent
   ],
   imports: [
     BrowserModule
