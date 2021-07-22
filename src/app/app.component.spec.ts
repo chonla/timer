b50c50230e5582de848da29e3831e10211741c58
Chonlasith Jucksriporn <chonlasith@gmail.com>
2021-07-22 16:51:04 +0700

    Fix deps in App test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,18 @@
 import { of } from 'rxjs';
 import { AppComponent } from './app.component';
+import { TimerState } from './enums/timer-state.enum';
+import { AudioService } from './services/audio.service';
 import { SettingsService } from './services/settings.service';
+import { TimerService } from './services/timer.service';
 
 describe('AppComponent', () => {
   let component: AppComponent;
+  let mockSettings: SettingsService;
+  let mockTimer: TimerService;
+  let mockAudio: AudioService;
 
   beforeEach(() => {
-    const mockSettings = ({
+    mockSettings = ({
       onSettingChanged$: jest.fn().mockReturnValue(of({
         useSound: true,
         darkMode: false,
@@ -14,8 +20,20 @@ describe('AppComponent', () => {
         selectedTheme: ''
       }))
     } as unknown) as SettingsService;
+    mockTimer = ({
+      start: jest.fn(),
+      stop: jest.fn(),
+      pause: jest.fn(),
+      resume: jest.fn(),
+      setTicks: jest.fn(),
+      onStateChange$: jest.fn().mockReturnValue(of(TimerState.UNINITIALIZED)),
+    } as unknown) as TimerService;
+    mockAudio = ({
+      load: jest.fn(),
+      play: jest.fn(),
+    } as unknown) as AudioService;
 
-    component = new AppComponent(mockSettings);
+    component = new AppComponent(mockSettings, mockTimer, mockAudio);
   });
 
   it('should create', () => {
