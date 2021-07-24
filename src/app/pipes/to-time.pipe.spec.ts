6b6846b2dc3e39c530a9fcb4c04edd5baedf24e0
Chonlasith Jucksriporn <chonlasith@gmail.com>
2021-07-24 12:37:39 +0700

    Add tests

--- src/app/pipes/to-time.pipe.spec.ts
+++ src/app/pipes/to-time.pipe.spec.ts
@@ -1,8 +1,43 @@
 import { ToTimePipe } from './to-time.pipe';
 
 describe('ToTimePipe', () => {
+  let pipe: ToTimePipe;
+
+  beforeEach(() => {
+    pipe = new ToTimePipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new ToTimePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should render 1 digit second correctly', () => {
+    const result = pipe.transform(9);
+
+    expect(result).toBe('00:09');
+  });
+
+  it('should render 2 digits second correctly', () => {
+    const result = pipe.transform(19);
+
+    expect(result).toBe('00:19');
+  });
+
+  it('should render 1 digit minute correctly', () => {
+    const result = pipe.transform(60);
+
+    expect(result).toBe('01:00');
+  });
+
+  it('should render 2 digits minute correctly', () => {
+    const result = pipe.transform(600);
+
+    expect(result).toBe('10:00');
+  });
+
+  it('should render 2 digits minute and 2 digits second correctly', () => {
+    const result = pipe.transform(754);
+
+    expect(result).toBe('12:34');
   });
 });
