dcdc4273efdad75a2b88240bae065c9d6d0f1393
Chonlasith Jucksriporn <chonlasith@gmail.com>
2021-07-25 13:30:29 +0700

    Add test

--- src/app/services/svg.service.spec.ts
+++ src/app/services/svg.service.spec.ts
@@ -128,6 +128,24 @@ describe('SvgService', () => {
     expect(result).toEqual(`M${x0},${y0}A${radius},${radius},0,${(+(diffAngle >= Math.PI))},${clockwiseship},${(x + radius * Math.cos(endAngle))},${(y + radius * Math.sin(endAngle))}`);
   });
 
+  it('should draw an arc correctly when angle is negative', () => {
+    const x = 10;
+    const y = 20;
+    const radius = 40;
+    const startAngle = Math.PI + 1e-6;
+    const endAngle = 0;
+    const counterClockwise = false;
+    const clockwiseship = counterClockwise?0:1;
+    const dx = radius * Math.cos(startAngle);
+    const dy = radius * Math.sin(startAngle);
+    const x0 = x + dx;
+    const y0 = y + dy;
+    const diffAngle = endAngle - startAngle;
+
+    const result = service.circularArc(x, y, radius, startAngle, endAngle, counterClockwise);
+    expect(result).toEqual(`M${x0},${y0}A${radius},${radius},0,${(+(diffAngle >= Math.PI))},${clockwiseship},${(x + radius * Math.cos(endAngle))},${(y + radius * Math.sin(endAngle))}`);
+  });
+
   it('should throw an error if radius is negative', () => {
     const x = 0;
     const y = 0;
@@ -142,5 +160,23 @@ describe('SvgService', () => {
     expect(() => {
       service.circularArc(x, y, radius, startAngle, endAngle, counterClockwise);
     }).toThrowError(new Error('negative radius: -1'));
+  });
+
+  it('should draw an arc in counter clockwise direction', () => {
+    const x = 10;
+    const y = 20;
+    const radius = 40;
+    const startAngle = 0;
+    const endAngle = Math.PI + 1e-6;
+    const counterClockwise = true;
+    const clockwiseship = counterClockwise?0:1;
+    const dx = radius * Math.cos(startAngle);
+    const dy = radius * Math.sin(startAngle);
+    const x0 = x + dx;
+    const y0 = y + dy;
+    const diffAngle = startAngle - endAngle;
+
+    const result = service.circularArc(x, y, radius, startAngle, endAngle, counterClockwise);
+    expect(result).toEqual(`M${x0},${y0}A${radius},${radius},0,${(+(diffAngle >= Math.PI))},${clockwiseship},${(x + radius * Math.cos(endAngle))},${(y + radius * Math.sin(endAngle))}`);
   });
 });
